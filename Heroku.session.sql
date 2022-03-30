759a86cb7241cecfd780a671b53a067c0938dbde
David Larronda <david.larronda@gmail.com>
2022-03-30 17:22:06 -0300

    ja

---
+++ Heroku.session.sql
@@ -0,0 +1,16 @@
+-- CREATE TABLE noticias(
+--   id_noticia SERIAL PRIMARY KEY,
+--   titulo varchar(100) NOT NULL,
+--   conteudo text NOT NULL
+--   data_criacao TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE ‘utc+3’)
+-- )
+
+
+
+-- INSERT INTO noticias (
+--   titulo,
+--   conteudo
+-- ) VALUES (
+--   'Notícia Teste',
+--   'Texto da notícia de teste'
+-- ) 
